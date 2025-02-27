024a249af4722efc18a7fe2ad2a98c36a6085821
Rezhan Fatah <rezhanrebar@gmail.com>
2025-02-27 03:27:01 +0300

    Update part1.sql
    Added more queries

--- part1.sql
+++ part1.sql
@@ -35,4 +35,49 @@ CREATE TABLE Series(
     FOREIGN KEY (mpid) REFERENCES MotionPicture(id)
         ON DELETE CASCADE
 )
+CREATE TABLE People (
+    id INT NOT NULL,   
+    name VARCHAR(100),   
+    nationality VARCHAR(100),   
+    dob DATE,   
+    gender VARCHAR(1) 
+);
 
+CREATE TABLE Role(
+     mpid int NOT NULL, 
+     pid int NOT NULL,
+     role_name varchar(100) NOT NULL,
+     PRIMARY KEY (mpid, pid, role_name),
+     FOREIGN KEY (mpid) REFERENCES MotionPicture(id)
+         ON DELETE CASCADE,
+     FOREIGN KEY (pid) REFERENCES People(id)
+         ON DELETE CASCADE
+     );
+create table Award(
+    mpid int not null,  
+    pid int not null, 
+    award_name varchar(100) not null,  
+    award_year int not null,
+    Primary key(mpid, pid, award_name, award_year),
+    foreign key (mpid) references motionpicture(id) 
+    on delete cascade,
+    foreign key (pid) references people(id) 
+    on delete cascade );
+
+CREATE TABLE Genre(
+    mpid int NOT NULL,
+    genre_name varchar(100) NOT NULL,
+    PRIMARY KEY (mpid, genre_name),
+    FOREIGN KEY (mpid) REFERENCES MotionPicture(id)
+        ON DELETE CASCADE
+    );
+
+CREATE TABLE Location(
+    mpid INT NOT NULL,
+    zip INT NOT NULL,
+    city varchar(100),
+    country varchar(100),
+    PRIMARY KEY(mpid, zip),
+    FOREIGN KEY (mpid) REFERENCES MotionPicture(id)
+        ON DELETE CASCADE
+    );
